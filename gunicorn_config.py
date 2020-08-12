7030c70c41be4a0cbd6fd7ea86e74081fde23d86
ws_zhoujing <121531845@qq.com>
2020-08-12 16:50:40 +0800

    加入gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,8 +6,9 @@ from logging.handlers import WatchedFileHandler
 import multiprocessing
 
 bind = '0.0.0.0:5001'      # 绑定ip和端口号
+daemon = True
 backlog = 100                # 监听队列
-# chdir = '/workspace/django_inference/AppServer'  # gunicorn要切换到的目的工作目录
+chdir = '/root/albumy'  # gunicorn要切换到的目的工作目录
 timeout = 6      # Workers silent for more than this many seconds are killed and restarted.
 keepalive = 2   # The number of seconds to wait for requests on a Keep-Alive connection.
 graceful_timeout = 20   # Timeout for graceful workers restart.
@@ -19,8 +20,8 @@ limit_request_field_size = 1024     # Limit the allowed size of an HTTP request 
 
 workers = 1    # 进程数
 threads = 1     # 指定每个进程开启的线程数
-# loglevel = 'info' # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
-# access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'    # 设置gunicorn访问日志格式，错误日志无法设置
+loglevel = 'info' # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
+access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'    # 设置gunicorn访问日志格式，错误日志无法设置
 
 """
 其每个选项的含义如下：
@@ -38,5 +39,5 @@ D          request time in microseconds
 L          request time in decimal seconds
 p          process ID
 """
-# accesslog = "/workspace/django_inference/gunicorn_access.log"      #访问日志文件
-# errorlog = "/workspace/django_inference/gunicorn_error.log"        #错误日志文件
+accesslog = "/root/albumy/gunicorn_access.log"      #访问日志文件
+errorlog = "/root/albumy/gunicorn_error.log"        #错误日志文件
